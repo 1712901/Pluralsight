eee1389293a2b1877940dd6029a8dbab662b29f5
= <tranvi9x@gmail.com>
2020-10-22 12:40:43 +0700

    Chuyển sang StatelessWidget

--- lib/Page/HomePage.dart
+++ lib/Page/HomePage.dart
@@ -1,21 +1,6 @@
 import 'package:Pluralsight/Components/AppBar.dart';
 import 'package:flutter/material.dart';
 
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar:myAppbar(title: 'Home'),
-//     );
-//   }
-// }
-
 class HomePage extends StatelessWidget {
   final String title;
 
@@ -24,6 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black54,
       appBar: myAppbar(title: title),
     );
   }
