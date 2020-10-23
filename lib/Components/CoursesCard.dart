783f972174b6e8d8d86bf28d700d152c5aa4d88d
= <tranvi9x@gmail.com>
2020-10-23 10:13:39 +0700

    Tạo card cho danh sách khóa học ở màn hình Home

---
+++ lib/Components/CoursesCard.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class Courses extends StatefulWidget {
+  @override
+  _CoursesState createState() => _CoursesState();
+}
+
+class _CoursesState extends State<Courses> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            return courseCard(index: index);
+          }),
+    );
+  }
+
+  Widget courseCard({index}) {
+    return Container(
+      width: 220,
+      child: Card(
+        color: Colors.grey[800],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        colors: [
+                      Colors.black.withOpacity(0.4),
+                      Colors.black.withOpacity(0.2)
+                    ])),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    PopupMenuButton(
+                        offset: Offset(0, 35),
+                        icon: Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                        ),
+                        color: Colors.grey[800],
+                        itemBuilder: (BuildContext context) {
+                          return <PopupMenuEntry<FlatButton>>[
+                            PopupMenuItem(
+                                child: FlatButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Bookmark',
+                                      style: TextStyle(color: Colors.white),
+                                    ))),
+                            PopupMenuItem(
+                                child: FlatButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Add to channel',
+                                      style: TextStyle(color: Colors.white),
+                                    ))),
+                            PopupMenuItem(
+                                child: FlatButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Download',
+                                      style: TextStyle(color: Colors.white),
+                                    ))),
+                            PopupMenuItem(
+                                child: FlatButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Share',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(color: Colors.white),
+                                    ))),
+                          ];
+                        }),
+                    IconButton(
+                        alignment: Alignment.bottomRight,
+                        icon: Icon(
+                          Icons.bookmark,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {}),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              //width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Angular Fundamentals',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Joe Eames',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      child: Text(
+                        'Intermediate Feb_2019 9h35m',
+                        style: TextStyle(color: Colors.grey),
+                        overflow: TextOverflow.visible,
+                        maxLines: 1,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        RatingBarIndicator(
+                          rating: 3.45,
+                          itemBuilder: (context, index) => Icon(
+                            Icons.star,
+                            //size: 15,
+                            color: Colors.amber,
+                          ),
+                          itemCount: 5,
+                          itemSize: 15.0,
+                          direction: Axis.horizontal,
+                        ),
+                        Text(
+                          '(555)',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Page/HomePage.dart
+++ lib/Page/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:Pluralsight/Components/AppBar.dart';
+import 'package:Pluralsight/Components/CoursesCard.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,6 +12,68 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black54,
       appBar: myAppbar(title: title),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/background.jpg'),
+                      fit: BoxFit.cover),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'Welcome to Pluraslight!',
+                        style: TextStyle(color: Colors.grey[400]),
+                      ),
+                      Text(
+                          'With Pluralsight, you can build and apply skills in top technologies. You have free access to skill IQ, Role IQ,a limited library of courses and a weekly rotation of new courses.',
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Software Development',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Text('see all'),
+                    label: Icon(
+                      Icons.navigate_next,
+                      color: Colors.grey,
+                    ),
+                    textColor: Colors.grey,
+                  )
+                ],
+              ),
+              Courses(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
