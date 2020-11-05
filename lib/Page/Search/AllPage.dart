80a3249af48e95ba7a53c4ec28a9392335d17427
= <tranvi9x@gmail.com>
2020-11-05 10:34:54 +0700

    Thêm màn danh sách các kết quả tìm kiếm

--- lib/Page/Search/AllPage.dart
+++ lib/Page/Search/AllPage.dart
@@ -3,9 +3,21 @@ import 'package:Pluralsight/Components/CourseListTile.dart';
 import 'package:Pluralsight/Components/PathListTitle.dart';
 import 'package:flutter/material.dart';
 
-class AllPage extends StatelessWidget {
+class AllPage extends StatefulWidget {
+  final Function(int index) funCallBack;
+
+  AllPage({this.funCallBack});
+
+  @override
+  _AllPageState createState() => _AllPageState(funCallBack: funCallBack);
+}
+
+class _AllPageState extends State<AllPage> {
   List<String> list = ['1', '2', '3', '4'];
+  final Function(int index) funCallBack;
+  _AllPageState({this.funCallBack});
   final int numItems = 4;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,12 +37,15 @@ class AllPage extends StatelessWidget {
                       style: TextStyle(color: Colors.white),
                     ),
                     FlatButton.icon(
-                      onPressed: () {},
+                      onPressed: () {
+                        funCallBack(1);
+                      },
                       icon: Text(
                         'See all',
                       ),
                       label: Icon(Icons.navigate_next),
                       textColor: Colors.grey,
+                      //onPressed: funCallBack(1),
                     )
                   ],
                 ),
@@ -52,7 +67,9 @@ class AllPage extends StatelessWidget {
                       style: TextStyle(color: Colors.white),
                     ),
                     FlatButton.icon(
-                      onPressed: () {},
+                      onPressed: () {
+                        funCallBack(2);
+                      },
                       icon: Text(
                         'See all',
                       ),
@@ -79,7 +96,9 @@ class AllPage extends StatelessWidget {
                       style: TextStyle(color: Colors.white),
                     ),
                     FlatButton.icon(
-                      onPressed: () {},
+                      onPressed: () {
+                        funCallBack(3);
+                      },
                       icon: Text(
                         'See all',
                       ),
--- lib/Page/SearchPage.dart
+++ lib/Page/SearchPage.dart
@@ -9,12 +9,42 @@ class SearchPage extends StatefulWidget {
   _SearchPageState createState() => _SearchPageState();
 }
 
-class _SearchPageState extends State<SearchPage> {
-  int initPage = 0;
-  final _controller = PageController(
-    initialPage: 0,
-  );
+class _SearchPageState extends State<SearchPage>
+    with SingleTickerProviderStateMixin {
+  //int initPage = 0;
+  TabController _tabController;
+  String query = "";
+  bool searchResult = false;
+  bool finding = false;
+
+  List<String> recents = ["Android", "C#", "Java"];
+  List<String> dataSearch = [
+    "Android",
+    "C#",
+    "Java",
+    "HTML/CSS",
+    "Nodejs",
+    "C/C++",
+    "Design parttern"
+  ];
+  List<String> showData;
+  TextEditingController _controller;
+
   final List<String> listTile = ['ALL', 'COURSES', 'PATHS', 'AUTHOURS'];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: listTile.length);
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,13 +56,25 @@ class _SearchPageState extends State<SearchPage> {
           backgroundColor: Colors.grey[800],
           elevation: 0,
           title: TextField(
+            controller: _controller,
+            textAlignVertical: TextAlignVertical.center,
             decoration: InputDecoration(
               hintText: 'Search...',
-              isDense: true,
-              contentPadding: EdgeInsets.all(10),
               hintStyle:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
               fillColor: Colors.white,
+              suffixIcon: IconButton(
+                icon: Icon(
+                  Icons.clear,
+                  color: Colors.grey,
+                ),
+                onPressed: () {
+                  _controller.clear();
+                  setState(() {
+                    query = "";
+                  });
+                },
+              ),
               focusedBorder: UnderlineInputBorder(
                   borderSide: BorderSide(color: Colors.black87)),
               enabledBorder: UnderlineInputBorder(
@@ -43,30 +85,125 @@ class _SearchPageState extends State<SearchPage> {
               ),
             ),
             style: TextStyle(color: Colors.white),
+            onChanged: onChangeText,
+            onSubmitted: onSubmitted,
           ),
-          bottom: TabBar(
-            //indicatorSize: TabBarIndicatorSize.label,
-            indicator: BoxDecoration(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10)),
-              color: Colors.black
-            ),
-            labelPadding: EdgeInsets.all(10),
-              tabs: [
-              Text('ALL'),
-              Text('COURSES'),
-              Text('PATHS'),
-              Text('AUTHORS'),
-            ]),
+          bottom: !searchResult ? null : tabBar(),
         ),
-        body: TabBarView(
-          children: [
-            AllPage(),
-            CoursesPage(),
-            PathPage(),
-            AuthourPage(),
-          ],
-        ),
+        body: !searchResult ? searchView() : tabBarView(),
       ),
     );
+  }
+
+  Widget searchView() {
+    return finding ? searchResultView() : searchRecentView();
+  }
+
+  Widget tabBarView() {
+    return TabBarView(
+      controller: _tabController,
+      children: [
+        AllPage(
+          funCallBack: (index) {
+            _tabController.animateTo(index);
+          },
+        ),
+        CoursesPage(),
+        PathPage(),
+        AuthourPage(),
+      ],
+    );
+  }
+
+  Widget tabBar() {
+    return TabBar(
+        controller: _tabController,
+        //indicatorSize: TabBarIndicatorSize.label,
+        indicator: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+            color: Colors.black),
+        labelPadding: EdgeInsets.all(10),
+        tabs: [
+          Text('ALL'),
+          Text('COURSES'),
+          Text('PATHS'),
+          Text('AUTHORS'),
+        ]);
+  }
+
+  void onChangeText(String text) {
+    if (text.isEmpty) {
+      setState(() {
+        finding = false;
+      });
+    } else {
+      setState(() {
+        searchResult = false;
+        finding = true;
+        showData = dataSearch
+            .where((element) =>
+                element.toUpperCase().startsWith(text.toUpperCase()))
+            .toList();
+      });
+    }
+  }
+
+  void onSubmitted(String text) {
+    setState(() {
+      recents.add(text);
+    });
+  }
+
+  Widget searchRecentView() {
+    return ListView.builder(
+        itemCount: recents.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              setState(() {
+                searchResult = true;
+                _controller.text = recents[index].toString();
+              });
+            },
+            leading: Icon(
+              Icons.history,
+              color: Colors.white,
+            ),
+            title: Text(
+              recents[index].toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.close,
+                color: Colors.grey,
+              ),
+              onPressed: () {
+                recents.removeAt(index);
+                setState(() {});
+              },
+            ),
+          );
+        });
+  }
+
+  Widget searchResultView() {
+    return ListView.builder(
+        itemCount: showData.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              setState(() {
+                searchResult = true;
+                _controller.text = showData[index].toString();
+              });
+            },
+            title: Text(
+              showData[index].toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        });
   }
 }
